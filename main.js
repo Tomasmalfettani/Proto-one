0ef239c281c78a7d92f90415a57e4e28119ef8fa
Tomas Malfettani <tomasmalfettani@gmail.com>
2023-07-14 16:03:10 -0300

    Es responsive, falta alinear componentes en el Eje x del menu

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-window.addEventListener("scroll", function () {
+document.addEventListener("DOMContentLoaded", function () {
     var header = document.querySelector("header");
-    header.classList.toggle("abajo", window.scrollY > 0);
+    var menuToggle = document.querySelector(".menu-toggle");
+    var sidebar = document.querySelector(".sidebar");
+
+    menuToggle.addEventListener("click", function () {
+        sidebar.classList.toggle("open");
+        menuToggle.classList.toggle("open");
+    });
+
+    window.addEventListener("scroll", function () {
+        header.classList.toggle("abajo", window.scrollY > 0);
+    });
 });
