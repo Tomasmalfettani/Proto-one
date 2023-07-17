cbb10fd8dbf1ce1d2be29200023e3cdb258f5623
Tomas Malfettani <tomasmalfettani@gmail.com>
2023-07-17 14:24:16 -0300

    Sidebar sombra, sidebar cruz, icono de wpp

--- main.js
+++ main.js
@@ -2,13 +2,29 @@ document.addEventListener("DOMContentLoaded", function () {
     var header = document.querySelector("header");
     var menuToggle = document.querySelector(".menu-toggle");
     var sidebar = document.querySelector(".sidebar");
+    var navMenu = document.querySelector("nav");
 
     menuToggle.addEventListener("click", function () {
         sidebar.classList.toggle("open");
         menuToggle.classList.toggle("open");
+        navMenu.classList.toggle("menu-open");
+
+        if (sidebar.classList.contains("open")) {
+            if (!header.classList.contains("abajo")) {
+                header.classList.add("abajo");
+            }
+            menuToggle.innerHTML = '<i class="bi bi-x"></i>';
+        } else {
+            if (window.scrollY === 0) {
+                header.classList.remove("abajo");
+            }
+            menuToggle.innerHTML = '<i class="bi bi-list"></i>';
+        }
     });
 
     window.addEventListener("scroll", function () {
-        header.classList.toggle("abajo", window.scrollY > 0);
+        if (!sidebar.classList.contains("open")) {
+            header.classList.toggle("abajo", window.scrollY > 0);
+        }
     });
 });
