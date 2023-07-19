bcc4bed38d6b18e385467c114c19eada9c43c64d
Tomas Malfettani <tomasmalfettani@gmail.com>
2023-07-19 16:22:03 -0300

    Evento cards, no me convence

--- main.js
+++ main.js
@@ -28,3 +28,42 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+const descriptionContainer = document.querySelector('.description-container');
+const cards = document.querySelectorAll('.cards');
+
+function toggleDescription(index) {
+    if (descriptionContainer.classList.contains('show') && descriptionContainer.dataset.cardIndex === index.toString()) {
+        descriptionContainer.classList.remove('show');
+        descriptionContainer.innerHTML = '';
+    } else {
+        descriptionContainer.innerHTML = getDescription(index);
+        descriptionContainer.dataset.cardIndex = index.toString();
+        descriptionContainer.classList.add('show');
+    }
+}
+
+function getDescription(index) {
+    const descriptions = [
+        '<div class="description-content">' +
+        '<div class="description-image">' +
+        '<img src="img/desarrolloWebProto.png" alt="Desarrollo Web Empresarial" class="foto-cards">' +
+        '</div>' +
+        '<div class="description-text">' +
+        '<h3>Pagina Web Empresarial</h3>' +
+        '<p>If your business does not yet have a website, then it is time to have an online presence that communicates the brand of your company, as well as that displays information about services or products.</p>' +
+        '</div>' +
+        '</div>',
+        '<div class="description-content">' +
+        '<div class="description-image">' +
+        '<img src="" alt="">' +
+        '</div>' +
+        '<div class="description-text">' +
+        '<p>Descripción del servicio 2</p>' +
+        '</div>' +
+        '</div>',
+        // Agrega más descripciones aquí
+    ];
+
+    return descriptions[index];
+}
