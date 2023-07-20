3350c928637bc99e4ea3d8ee94a6bb374bcb5db0
Tomas Malfettani <tomasmalfettani@gmail.com>
2023-07-20 12:06:29 -0300

    Evento active en las cards, descripcion de servicios, puse las cards en links

--- main.js
+++ main.js
@@ -28,3 +28,51 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 });
+
+const cards = document.querySelectorAll('.cards');
+
+cards.forEach(card => {
+    card.addEventListener('click', () => {
+        cards.forEach(card => {
+            card.classList.remove('active');
+        });
+
+        card.classList.add('active');
+    });
+});
+
+function changeDescription(index) {
+    var cards = document.querySelectorAll('.cards');
+    var descripcion = document.getElementById('descripcion');
+
+    cards.forEach(function (card) {
+        card.classList.remove('active');
+    });
+
+    cards[index].classList.add('active');
+
+    switch (index) {
+        case 0:
+            descripcion.innerHTML = '<h3>Descripción del servicio 1</h3><p>LLorem ipsum dolor sit amet consectetur, adipisicing elit. Earum maxime, iste ut quia et minimaoptio deserunt rem quis perspiciatis harum esse cum ratione est reiciendis eius, vero, molestiasplaceat.</p>';
+            break;
+        case 1:
+            descripcion.innerHTML = '<h3>Descripción del servicio 2</h3><p>Aquí va la descripción del servicio 2.</p>';
+            break;
+        case 2:
+            descripcion.innerHTML = '<h3>Descripción del servicio 3</h3><p>Aquí va la descripción del servicio 3.</p>';
+            break;
+        case 3:
+            descripcion.innerHTML = '<h3>Descripción del servicio 4</h3><p>Aquí va la descripción del servicio 4.</p>';
+            break;
+        case 4:
+            descripcion.innerHTML = '<h3>Descripción del servicio 5</h3><p>Aquí va la descripción del servicio 5.</p>';
+            break;
+        case 5:
+            descripcion.innerHTML = '<h3>Descripción del servicio 6</h3><p>Aquí va la descripción del servicio 6.</p>';
+            break;
+        default:
+            descripcion.innerHTML = '<h3>Descripción del servicio 1</h3><p>Aquí va la descripción del servicio 1.</p>';
+            break;
+    }
+
+}
