c7ad9a03d405a0abfaf160e9322151463154ecf4
Tomas Malfettani <tomasmalfettani@gmail.com>
2023-07-25 15:30:52 -0300

    Chau why

--- main.js
+++ main.js
@@ -77,14 +77,3 @@ function changeDescription(index) {
             break;
     }
 }
-
-// Función para verificar si el elemento está visible en la ventana del navegador
-function isElementInViewport(el) {
-    const rect = el.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
